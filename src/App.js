d327f89efa8a7d16ec13b52b06ff2245986ff148
igknighton <trent.knighton@gmail.com>
2022-09-08 17:45:29 -0500

    static design implementation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
-
+import divider from './images/pattern-divider-desktop.svg';
+import dice from './images/icon-dice.svg';
 function App() {
+
+
+    const randomQuote = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores beatae delectus fugiat iste, quasi quo?';
+    const adviceNumber = 117;
   return (
-    <div className="App">
-      Advice Generator
-    </div>
+          <div className="container">
+              <div className="adviceNumber">
+                  Advice #{adviceNumber}
+              </div>
+              {randomQuote}
+              <img src={divider} width={444} height={16} alt={'divider'}/>
+              <div className="button">
+                  <img src={dice} className="button" alt={'dice'}/>
+              </div>
+          </div>
   );
 }
 
