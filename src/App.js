c590fe5104674c65433e118e5aa62272462c3ee2
igknighton <trent.knighton@gmail.com>
2022-09-08 17:52:42 -0500

    api integration

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
 import './App.css';
 import divider from './images/pattern-divider-desktop.svg';
 import dice from './images/icon-dice.svg';
+import {useState} from "react";
 function App() {
 
 
-    const randomQuote = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores beatae delectus fugiat iste, quasi quo?';
-    const adviceNumber = 117;
+    const [quote,setQuote] = useState('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores beatae delectus fugiat iste, quasi quo?');
+    const [adviceNumber,setAdviceNumber ] = useState('');
+
+    const loadQuote = async () => {
+        fetch('https://api.adviceslip.com/advice')
+            .then(response => response.json())
+            .then(data => {
+                const {id,advice} = data.slip;
+                setQuote(advice);
+                setAdviceNumber(id);
+                }
+            )
+            .catch(err => console.error(err));
+    }
+
+
   return (
           <div className="container">
               <div className="adviceNumber">
                   Advice #{adviceNumber}
               </div>
-              {randomQuote}
+              {quote}
               <img src={divider} width={444} height={16} alt={'divider'}/>
               <div className="button">
-                  <img src={dice} className="button" alt={'dice'}/>
+                  <img src={dice} onClick={loadQuote} className="button" alt={'dice'}/>
               </div>
           </div>
   );
